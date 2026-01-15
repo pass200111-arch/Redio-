f16b5be5cddd0f698dcedb13b3d47ee2ddff3331
pass200111-arch <pass200111@gmail.com>
2026-01-16 01:48:12 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-const CACHE="radio-v1";
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html"])));
-  self.skipWaiting();
+const CACHE_NAME = "my-radio-category";
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(["./index.html","./manifest.json"])));
 });
-self.addEventListener("fetch",e=>{
+self.addEventListener("fetch", e => {
   if(e.request.url.includes(".mp3")||e.request.url.includes(".m3u8")) return;
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
